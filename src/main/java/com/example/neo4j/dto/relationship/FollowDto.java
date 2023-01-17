144928b8ac723e5b5b050f23475390b600021ed5
JeonghyeonK <6843wjdgus@naver.com>
2023-01-18 04:55:41 +0900

    test

--- src/main/java/com/example/neo4j/dto/relationship/FollowDto.java
+++ src/main/java/com/example/neo4j/dto/relationship/FollowDto.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class FollowDto {
-	private Long following_id;
-	private Long followed_id;
+	private Long followingId;
+	private Long followedId;
 }
-//teste
+//test
